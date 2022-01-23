42818733b227c56e1a31197b88dbb18c54d50632
Aman kumar <80037371+Amantomar81@users.noreply.github.com>
2022-01-24 01:19:27 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+public class ReverseString {
+    public static void Rev(String s,int indx){
+        if (indx == 0){
+            System.out.println(s.charAt(indx));
+            return;
+        }
+        System.out.print(s.charAt(indx));
+        Rev(s,indx-1);
+    }
+    public static void main(String[] args) {
+        String s = "abcd";
+        System.out.println("abcd");
+        Rev("abcd",s.length()-1);
+    }
+}
